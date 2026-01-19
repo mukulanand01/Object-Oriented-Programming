a15ededebe1846d39015cc07079fb50832876f15
Mukul Anand <mukulanand021@gmail.com>
2026-01-20 00:01:45 +0530

    Create functionoverridingrtp.cpp

---
+++ functionoverridingrtp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal sound karta hai" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog bhonkta hai" << endl;
+    }
+};
+
+int main() {
+    Animal* a;
+    Dog d;
+    a = &d;
+    a->sound();
+    return 0;
+}
+
+// Run time polymrophis function over riding
